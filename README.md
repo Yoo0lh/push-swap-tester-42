6de3a38d9f13c9274bfaf6d417a744815806cb9d
Hamza Lahrach <hlahrach@e2r1p8.1337.ma>
2022-02-01 22:21:30 +0100

    simple edit

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ this tester count the number of operation that made by your programme and test i
 <pre>git clone git@github.com:Yoo0lh/push_swap_tester.git </br>./push_swap_tester.sh [number of argument]
 </pre>
 ### example :
-this example will run the tester with 100 number of argument
-<pre> ./push_swap_tester.sh 100 </pre>
-<img src="screen_shot.png">
+this example will run the tester with 5 number of argument
+<pre> ./push_swap_tester.sh 5 </pre>
+<img src="img/Screen%20Shot%202022-02-01%20at%2010.12.57%20PM.png">
 
 # Contribution
 If you noticed something wrong with the code or if you'd like to see a new feature, you can submit an issue. If you'd like to contribute please submit a pull request. 
--- push_swap_tester.sh
+++ push_swap_tester.sh
@@ -2,7 +2,8 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 idont=`tput setaf 6`
-for i in {1..40}; do  echo "$yellow---------- ${idont}TEST $i ${yellow}----------------------------------------";
+(make fclean -C ../ && make -C ../) > /dev/null
+for i in {1..10}; do  echo "$yellow================= ${idont}TEST $i ${yellow}=================================";
     ARG="$(python random.numbers.py $1 | tr '\n' ' ' > ./test_files/test_$i.txt; cat ./test_files/test_$i.txt)";
 if [ "$( ../push_swap $ARG | ./Tester $ARG)" = "OK" ] ; then 
     echo "${green}R -->[OK]\nN --> $(../push_swap $ARG | wc -l | awk '{printf $1}')"
@@ -11,7 +12,7 @@ else
 fi
 sleep 0.5
 done;
-echo "${yellow}------------ ${red}END ${yellow}------------------------------------------"
+echo "${yellow}=================  ${red}END ${yellow}================================="
 echo "\n"
 echo "${red}NOTE 1: ${yellow}if you have ${red}KO ${yellow}you can check (./test_file) to see the example that we use to check your program"
 echo "${red}NOTE 2: ${yellow}if you run the tester again without modifing your code and you get ${green}OK ${yellow}taht's because the \n\ttester generate a new numbres"
